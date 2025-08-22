9413a0b7ab33bf4f947eb370f9bfefaad2647740
Elburz Sorkhabi <elburz@sorkhabi.io>
2025-08-22 15:42:12 -0400

    debugging

--- api/import-users.js
+++ api/import-users.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
 
   const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { require: true, rejectUnauthorized: false }
   });
 
   try {
